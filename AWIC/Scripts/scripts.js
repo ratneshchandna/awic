655c211d55de6cc0a195ac4fe2f46b3eedb240e2
Ratnesh Chandna <ratneshchandna@gmail.com>
2015-04-15 04:07:39 -0500

    - Cleaned general.css

--- AWIC/Scripts/scripts.js
+++ AWIC/Scripts/scripts.js
@@ -1,66 +1,11 @@
 "use strict";
 
 function InitHome() {
-
     $('#slider').flexslider({
-        animation: "slide",
+        animation: "fade",
         animationLoop: true,
+        pauseOnAction: false,
         prevText: '',
         nextText: ''
     });
-
-    $('#photo-slider').flexslider({
-        animation: "slide",
-        animationLoop: true,
-        prevText: '',
-        nextText: '',
-        itemMargin: 10
-    });
-
-    $('section.news .photos h3 .control .left').click(function() {
-        var slider = $('#photo-slider').data('flexslider');
-        slider.flexslider("prev");
-    });
-
-    $('section.news .photos h3 .control .right').click(function() {
-        var slider = $('#photo-slider').data('flexslider');
-        slider.flexslider("next");
-    });
-
 }
-
-
-function InitBlog() {
-
-    $('#photostream').flexslider({
-        animation: "slide",
-        animationLoop: true,
-        prevText: '',
-        nextText: ''
-    });
-
-    $('aside.blog #photostream h3 .control .left').click(function() {
-        var slider = $('#photostream').data('flexslider');
-        slider.flexslider("prev");
-    });
-
-    $('aside.blog #photostream h3 .control .right').click(function() {
-        var slider = $('#photostream').data('flexslider');
-        slider.flexslider("next");
-    });
-
-
-    $('#aside-dynamic .menu > div').click(function() {
-        var attr = $(this).attr('data-attr');
-
-        $('#aside-dynamic .menu > div').removeClass('active');
-        $(this).addClass('active');
-
-
-        $('#aside-dynamic .content').css('display','none');
-        $('#aside-dynamic .content.' + attr).css('display','block');
-    });
-
-}
-
-
